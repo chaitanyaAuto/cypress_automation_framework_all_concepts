a6392f6e684209b944670018a1ae62aa94e04243
chaitanyaAuto <155298227+chaitanyaAuto@users.noreply.github.com>
2026-02-01 10:55:41 +0530

    Add files via upload

---
+++ cypress.config.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  reporter:'cypress-mochawesome-reporter',
+  e2e: {
+    setupNodeEvents(on, config) {
+      this.screenshotOnRunFailure=true,
+      require('cypress-mochawesome-reporter/plugin')(on);
+      // implement node event listeners here
+    },
+  },
+});
+
+//cypress.config.js
+
+
